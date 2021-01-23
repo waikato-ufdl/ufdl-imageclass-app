7fa472ee9f83ad7a4194abb4a8fb10c6fe322449
Yug Patel <yp61@students.waikato.ac.nz>
2021-01-23 19:01:13 +1300

    The image is now being loaded into the holder using a filepath. First we check if the image has been cached, if so, then retrieve cached image. Else, retrieve full image.
    If an image is clicked while action mode if off, open it up in an image viewer. If the user swipes the image viewer, transition to the previous image or next image depending on swipe direction. If a user swipes in a vertical manner, then dismiss the image viewer and return to the image grid recyclerview.

--- app/src/main/java/io/github/waikato_ufdl/ui/images/ImageListAdapter.java
+++ app/src/main/java/io/github/waikato_ufdl/ui/images/ImageListAdapter.java
@@ -3,6 +3,7 @@ package io.github.waikato_ufdl.ui.images;
 import android.content.Context;
 import android.graphics.Color;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -14,15 +15,19 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.bumptech.glide.Glide;
+
 import io.github.waikato_ufdl.MainActivity;
 import io.github.waikato_ufdl.R;
 import io.github.waikato_ufdl.ui.settings.Utility;
 import com.github.waikatoufdl.ufdl4j.action.ImageClassificationDatasets;
+import com.stfalcon.imageviewer.StfalconImageViewer;
+import java.io.File;
 import java.util.ArrayList;
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
@@ -36,27 +41,29 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.View
     ImagesFragmentViewModel imagesViewModel;
     ImageClassificationDatasets action;
     private int datasetPK;
-
+    private String datasetName;
+    public StfalconImageViewer<ClassifiedImage> viewer;
 
     /**
      * Constructor for image list adapter
-     * @param fragment the images fragment
-     * @param context the context
+     *
+     * @param fragment  the images fragment
+     * @param context   the context
      * @param imageList the list of images within a particular data set
-     * @param action  the action which will be used to perform API requests specifically on Image Classification datasets
+     * @param action    the action which will be used to perform API requests specifically on Image Classification datasets
      * @param datasetPK the particular dataset's primary key
      */
-    public ImageListAdapter(ImagesFragment fragment, Context context, ArrayList<ClassifiedImage> imageList, ImageClassificationDatasets action, int datasetPK)
-    {
+    public ImageListAdapter(ImagesFragment fragment, Context context, ArrayList<ClassifiedImage> imageList, ImageClassificationDatasets action, int datasetPK, String datasetName) {
         this.fragment = fragment;
         this.context = context;
         this.inflater = LayoutInflater.from(context);
         this.imageList = imageList;
         this.action = action;
         this.datasetPK = datasetPK;
+        this.datasetName = datasetName;
 
         isActionMode = false;
-        selectedImages =  new ArrayList<>();
+        selectedImages = new ArrayList<>();
     }
 
     @NonNull
@@ -66,7 +73,7 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.View
         View view = inflater.inflate(R.layout.image_display, parent, false);
 
         //initialise view model
-        imagesViewModel =  ViewModelProviders.of(fragment).get(ImagesFragmentViewModel.class);
+        imagesViewModel = new ViewModelProvider(fragment).get(ImagesFragmentViewModel.class);
         return new ViewHolder(view);
     }
 
@@ -76,39 +83,41 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.View
 
         ClassifiedImage image = imageList.get(position);
 
-        if(image != null)
-        {
+        if (image != null) {
             //set the classification label
-            holder.classification.setText(image.getClassification());
+            holder.classification.setText(image.getClassificationLabel());
 
-            Glide.with(context).clear(holder.image);
+            String imagePath = (image.getCachedFilePath() != null) ? image.getCachedFilePath() : image.getFullImageFilePath();
 
             //use glide to load image into the image view for display
             Glide.with(context)
-                    .asBitmap()
-                    .load(image.getImageArray())
+                    .load(new File(imagePath))
                     .placeholder(R.drawable.progress_animation)
                     .into(holder.image);
+
+            holder.image.setTransitionName(imagePath);
         }
 
-        if(image.isSelected())
-        {
+        if (image.isSelected()) {
             holder.checkBox.setChecked(true);
             holder.checkBox.setVisibility(View.VISIBLE);
 
             //set textview background colour
             holder.classification.setBackgroundColor(Color.RED);
             holder.classification.setTextColor(Color.WHITE);
-        }
-        else
-        {
+        } else {
             holder.checkBox.setChecked(false);
             holder.checkBox.setVisibility(View.GONE);
 
-            //set textview background colour
-            holder.classification.setBackgroundColor(Color.TRANSPARENT);
 
-            if(!Utility.loadDarkModeState()) {
+            TypedValue value = new TypedValue();
+            context.getTheme().resolveAttribute(R.attr.imageLabelBackground, value, true);
+
+            //set textview background colour
+            holder.classification.setBackgroundColor(value.data);
+
+
+            if (!Utility.loadDarkModeState()) {
                 holder.classification.setTextColor(Color.BLACK);
             }
         }
@@ -116,7 +125,7 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.View
         //on long press of an image we want to invoke the action mode
         holder.itemView.setOnLongClickListener(v -> {
             //if action mode in not enabled, initialise action mode
-            if(!isActionMode) {
+            if (!isActionMode) {
                 //initialise Action mode
                 ActionMode.Callback callback = new ActionMode.Callback() {
 
@@ -154,8 +163,7 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.View
                         int id = item.getItemId();
 
                         //check which menu item was clicked
-                        switch (id)
-                        {
+                        switch (id) {
                             case R.id.action_delete:
                                 //when user presses delete
                                 deleteConfirmation(mode);
@@ -177,8 +185,8 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.View
                         isActionMode = false;
 
                         //go through each selected image & update it's isSelected to false
-                        for (ClassifiedImage image1 : selectedImages) {
-                            image1.setSelected(false);
+                        for (ClassifiedImage selectedImage : selectedImages) {
+                            selectedImage.setSelected(false);
                         }
 
                         //clear selected images list & notify adapter
@@ -188,9 +196,7 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.View
                 };
                 //Start action mode
                 ((MainActivity) v.getContext()).startActionMode(callback);
-            }
-            else
-            {
+            } else {
                 //when action mode is already enabled call the onImageSelect method
                 onImageSelect(holder);
             }
@@ -199,21 +205,57 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.View
 
         holder.itemView.setOnClickListener(v -> {
             //if action mode is enabled
-            if(isActionMode)
-            {
+            if (isActionMode) {
                 //if a user selects an image,
                 onImageSelect(holder);
-            }
-            else
-            {
+            } else {
                 //an image has been selected (but not in selection mode)
                 Log.v("TAG", holder.getAdapterPosition() + " ");
+
+                View view = LayoutInflater.from(context).inflate(R.layout.pager_overlay_view, null, false);
+                TextView index = view.findViewById(R.id.imageIndex);
+                TextView label = view.findViewById(R.id.imageLabel);
+
+
+                index.setText((position + 1) + "/" + imageList.size());
+                label.setText(image.getClassificationLabel());
+
+                viewer = new StfalconImageViewer.Builder<>(context, imageList, (imageView, picture) ->
+                        Glide.with(context)
+                                .load((picture.getFullImageFilePath() != null) ? picture.getFullImageFilePath() : picture.getCachedFilePath())
+                                .placeholder(R.drawable.progress_animation)
+                                .into(imageView))
+                        .withStartPosition(position)
+                        .withTransitionFrom(holder.image)
+                        .withOverlayView(view)
+                        .withImageChangeListener(pos -> {
+
+                            viewer.updateImages(imageList);
+                            UpdateTransitionImage(pos);
+
+                            //update the image index position and classification label text
+                            if (position <= imageList.size()) {
+                                index.setText((pos + 1) + "/" + imageList.size());
+                                label.setText(imageList.get(pos).getClassificationLabel());
+                            }
+                        }).show();
             }
         });
     }
 
+    public void UpdateTransitionImage(int position) {
+        ViewHolder ImageViewHolder = (ViewHolder) fragment.recyclerView.findViewHolderForAdapterPosition(position);
+        if (ImageViewHolder != null && ImageViewHolder instanceof ViewHolder) {
+            ImageView imageView = ImageViewHolder.image;
+            if (imageView != null) {
+                viewer.updateTransitionImage(imageView);
+            }
+        }
+    }
+
     /**
      * Method to handle image selection/deselection
+     *
      * @param holder the view holder
      */
     private void onImageSelect(ViewHolder holder) {
@@ -221,14 +263,11 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.View
         ClassifiedImage image = imageList.get(holder.getAdapterPosition());
 
         //if an image has been selected
-        if(!image.isSelected())
-        {
+        if (!image.isSelected()) {
             //set its isSelected state to true & add the image to the selectedImages list
             image.setSelected(true);
             selectedImages.add(image);
-        }
-        else
-        {
+        } else {
             //else an image has been deselected so set isSelected to false and remove from selectedImages list
             image.setSelected(false);
             selectedImages.remove(image);
@@ -247,8 +286,7 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.View
     }
 
 
-    public class ViewHolder extends RecyclerView.ViewHolder
-    {
+    public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView image;
         TextView classification;
         CheckBox checkBox;
@@ -265,10 +303,10 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.View
 
     /**
      * A method to confirm the deletion process via a popup before deleting images
+     *
      * @param mode the action mode
      */
-    public void deleteConfirmation(ActionMode mode)
-    {
+    public void deleteConfirmation(ActionMode mode) {
         new SweetAlertDialog(context, SweetAlertDialog.WARNING_TYPE)
                 .setTitleText("Are you sure?")
                 .setContentText("You won't be able to recover the image(s) after deletion")
@@ -277,7 +315,7 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.View
                     sDialog.dismissWithAnimation();
 
                     //delete the selected images
-                    DeleteTask deleteImages = new DeleteTask(fragment, context, selectedImages, datasetPK, action, mode);
+                    DeleteTask deleteImages = new DeleteTask(fragment, context, selectedImages, datasetName, action, mode);
                     deleteImages.execute();
                 })
                 .setCancelButton("Cancel", sDialog -> {
@@ -290,11 +328,12 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.View
 
     /**
      * A method which displays a confirmation dialog prompting the user to confirm the action of reclassifying images
+     *
      * @param mode the action mode
      */
     public void confirmEditCategories(ActionMode mode) {
         final EditText editText = new EditText(context);
-                new SweetAlertDialog(context, SweetAlertDialog.WARNING_TYPE)
+        new SweetAlertDialog(context, SweetAlertDialog.WARNING_TYPE)
                 .setTitleText("Reclassify all selected images as: ")
                 .setConfirmText("Reclassify")
                 .setCustomView(editText)
@@ -302,14 +341,14 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.View
                     String newClassification = editText.getText().toString().trim();
 
                     //if a value has been entered
-                    if(newClassification.length() > 0) {
-                        sweetAlertDialog.dismissWithAnimation();
+                    if (newClassification.length() > 0) {
+                        sweetAlertDialog.dismiss();
 
                         //reclassify selected images
-                        ReclassifyTask editCategoriesTask = new ReclassifyTask(fragment, context, selectedImages, newClassification, datasetPK, action, mode);
+                        ReclassifyTask editCategoriesTask = new ReclassifyTask(fragment, context, selectedImages, newClassification, datasetName, action, mode);
                         editCategoriesTask.execute();
-                    }
-                    else
+
+                    } else
                         editText.setError("Please enter a classification label");
                 })
                 .setCancelButton("Cancel", sDialog -> {
@@ -321,11 +360,12 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.View
 
     /**
      * A method to switch the adapter list to show another list. This will be used to switch between the full sized list and the filtered list.
+     *
      * @param list
      */
-    public void searchCategory(ArrayList<ClassifiedImage> list)
-    {
+    public void searchCategory(ArrayList<ClassifiedImage> list) {
         imageList = list;
         notifyDataSetChanged();
     }
 }
+
