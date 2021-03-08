2f5092d67cdc17187e2ce58974e384c6fcbd17c2
Yug Patel <yp61@students.waikato.ac.nz>
2021-03-08 14:22:10 +1300

    Created classes to encapsulate dataset operations and image operations.

---
+++ app/src/main/java/io/github/waikato_ufdl/DatasetOperations.java
@@ -0,0 +1,364 @@
+package io.github.waikato_ufdl;
+
+import android.content.Context;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.github.waikatoufdl.ufdl4j.action.Datasets;
+import com.github.waikatoufdl.ufdl4j.action.ImageClassificationDatasets;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+
+import id.zelory.compressor.Compressor;
+import io.github.waikato_ufdl.ui.manage.ImageDataset;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+/***
+ * Encapsulates dataset operations for creating, updating and deleting datasets from both the local and backend databases.
+ */
+
+public class DatasetOperations {
+    /***
+     * Inserts a image dataset to the local SQLite database with a sync status of CREATE and then attempts to make an API request to create the dataset on the backend.
+     * @param dbManager the local database manager
+     * @param name the name of the dataset
+     * @param description the dataset description
+     * @param project the project pk which the dataset belongs to
+     * @param license the license pk of the dataset
+     * @param isPublic whether the dataset is public
+     * @param tags the dataset's tags
+     */
+    public static void createDataset(DBManager dbManager, String name, String description, int project, int license, boolean isPublic, String tags) {
+        dbManager.insertLocalDataset(name, description, project, license, isPublic, tags);
+        if (SessionManager.isOnlineMode)
+            create(dbManager, name, description, project, license, isPublic, tags);
+    }
+
+    /***
+     * Performs an API request to create a dataset on the backend. If the request is successful, the dataset will be set to SYNCED in the local database.
+     * @param dbManager the local database manager
+     * @param name the name of the dataset
+     * @param description the dataset description
+     * @param project the project pk which the dataset belongs to
+     * @param license the license pk of the dataset
+     * @param isPublic whether the dataset is public
+     * @param tags the dataset's tags
+     */
+    public static void create(DBManager dbManager, String name, String description, int project, int license, boolean isPublic, String tags) {
+        Datasets.Dataset dataset;
+        try {
+            ImageClassificationDatasets action = SessionManager.getClient().action(ImageClassificationDatasets.class);
+            if ((dataset = action.create(name, description, project, license, isPublic, tags)) != null) {
+                dbManager.setDatasetSynced(dataset.getName());
+                dbManager.updateLocalDatasetAfterSync(dataset.getPK(), dataset.getName(), dataset.getDescription(), dataset.getProject(), dataset.getLicense(), dataset.isPublic(), dataset.getTags());
+            }
+        } catch (Exception e) {
+            Log.e("TAG", "Error while trying to upload database:\n" + e.getMessage());
+        }
+    }
+
+
+    /***
+     * Updates the dataset's information in the local database and sets its sync status to UPDATE and then attempts to perform an API request to update the dataset on the backend.
+     * @param dbManager the local database manager
+     * @param datasetPK the primary key of the dataset
+     * @param name the new name of the dataset
+     * @param oldDatasetName the current name of the dataset
+     * @param description the dataset description
+     * @param project the project pk which the dataset belongs to
+     * @param license the license pk of the dataset
+     * @param isPublic whether the dataset is public
+     * @param tags the dataset's tags
+     */
+    public static void updateDataset(DBManager dbManager, int datasetPK, String name, String oldDatasetName, String description, int project, int license, boolean isPublic, String tags) {
+        dbManager.updateDataset(oldDatasetName, name, description, project, license, isPublic, tags);
+
+        if (SessionManager.isOnlineMode)
+            update(dbManager, datasetPK, name, description, project, license, isPublic, tags);
+
+    }
+
+    /***
+     * Performs an API request to update an existing dataset on the backend. If the request is successful, the dataset will be set to SYNCED in the local database.
+     * @param dbManager the local database manager
+     * @param datasetPK the primary key of the dataset
+     * @param name the new name of the dataset
+     * @param description the dataset description
+     * @param project the project pk which the dataset belongs to
+     * @param license the license pk of the dataset
+     * @param isPublic whether the dataset is public
+     * @param tags the dataset's tags
+     */
+    public static void update(DBManager dbManager, int datasetPK, String name, String description, int project, int license, boolean isPublic, String tags) {
+        Datasets.Dataset dataset;
+        try {
+            ImageClassificationDatasets action = SessionManager.getClient().action(ImageClassificationDatasets.class);
+            if ((dataset = action.update(datasetPK, name, description, project, license, isPublic, tags)) != null) {
+                dbManager.setDatasetSynced(dataset.getName());
+            }
+        } catch (Exception e) {
+            Log.e("TAG", "API request failed -- Dataset Update Failed. Error:\n" + e.getMessage());
+        }
+    }
+
+    /***
+     * Sets the sync status of a dataset to DELETE in the local database and then attempts to perform an API request to delete the dataset from the backend.
+     * @param context the context
+     * @param dbManager the local database manager
+     * @param name the name of the dataset
+     */
+    public static void deleteDataset(Context context, DBManager dbManager, String name) {
+        dbManager.deleteDataset(name);
+
+        if (SessionManager.isOnlineMode)
+            delete(context, dbManager, name);
+
+    }
+
+    /***
+     * Performs an API request to attempt to delete a dataset from the backend. If the request is successful, the dataset will be removed from the local database.
+     * @param dbManager the local database manager
+     * @param name the name of the dataset
+     */
+    public static void delete(Context context, DBManager dbManager, String name) {
+        try {
+            ImageClassificationDatasets action = SessionManager.getClient().action(ImageClassificationDatasets.class);
+            int datasetPK = dbManager.getDatasetPK(name);
+
+            if (action.delete(datasetPK, true)) {
+                dbManager.setDatasetSynced(name);
+                dbManager.deleteLocalDataset(name);
+                deleteDatasetStorageDirectories(context, name);
+            }
+        } catch (Exception e) {
+            Log.e("TAG", "API request failed -- Dataset Deletion Failed. Error:\n" + e.getMessage());
+        }
+    }
+
+    /***
+     * Performs an API request to make a copy of an existing dataset.
+     * @param dbManager the local database manager
+     * @param dataset the name of the dataset to copy
+     * @param newDatasetName the name to give the copied dataset
+     * @return true if the dataset copy has been successfully created or false if the copy operation has failed.
+     */
+    public static boolean copyDataset(DBManager dbManager, ImageDataset dataset, String newDatasetName) {
+        try {
+            if (SessionManager.isOnlineMode) {
+                ImageClassificationDatasets action = SessionManager.getClient().action(ImageClassificationDatasets.class);
+                //create a copy of the dataset
+                if (action.copy(dataset.getPK(), newDatasetName)) {
+                    int pk = action.load(newDatasetName).getPK();
+                    dbManager.insertSyncedDataset(pk, newDatasetName, dataset.getDescription(), dataset.getProject(), dataset.getLicense(), dataset.isPublic(), dataset.getTags());
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            Log.e("TAG", "API request failed -- Dataset Copy Failed. Error: \n" + e.getMessage());
+        }
+        return false;
+    }
+
+    /***
+     * Downloads the contents (images) of a dataset
+     * @param context the context
+     * @param dbManager the local database manager
+     * @param selectedDataset the dataset to download
+     * @param isCache true to compress images
+     * @param progressBar the progress bar to display
+     */
+    public static void downloadDataset(Context context, DBManager dbManager, ImageDataset selectedDataset, boolean isCache, ProgressBar progressBar) {
+        try {
+            ImageClassificationDatasets action = SessionManager.getClient().action(ImageClassificationDatasets.class);
+            int datasetPK = selectedDataset.getPK();
+            String datasetName = selectedDataset.getName();
+            String[] imageFileNames = action.load(datasetPK).getFiles();
+            File datasetFolder = getImageStorageDirectory(context, datasetName, isCache);
+
+            Observable.fromArray(imageFileNames)
+                    .map(filename -> {
+                        byte[] image = action.getFile(datasetPK, filename);
+                        List<String> allLabels = action.getCategories(datasetPK, filename);
+                        String classificationLabel = (allLabels.size() > 0) ? allLabels.get(0) : "Unlabelled";
+
+                        return downloadImage(context, dbManager, datasetFolder, filename, image, classificationLabel, isCache);
+                    })
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(getDownloadObserver(progressBar, imageFileNames.length));
+
+        } catch (Exception e) {
+            new Handler(Looper.getMainLooper()).post(() ->
+            {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(context, "Error occurred while downloading datasets: \n +" + e.getMessage(), Toast.LENGTH_LONG).show();
+            });
+        }
+    }
+
+    /***
+     * Downloads an image and stores it within an app-specific directory.
+     * @param context the context
+     * @param dbManager the local database manager
+     * @param datasetFolder the folder to store the image in
+     * @param filename the name of the image file
+     * @param image the image data
+     * @param classificationLabel the classification label of the image
+     * @param isCache whether the file is cache file or not
+     * @return true if image has been downloaded and stored or else false if an error occurred.
+     */
+    public static boolean downloadImage(Context context, DBManager dbManager, File datasetFolder, String filename, byte[] image, String classificationLabel, boolean isCache) {
+        try {
+            File file = new File(datasetFolder, filename);
+
+            if (!file.exists()) {
+                FileOutputStream fos = new FileOutputStream(file.getPath());
+                fos.write(image);
+                fos.close();
+            }
+
+            if (isCache) {
+                compressFile(context, file);
+                if (!dbManager.insertSyncedImage(filename, classificationLabel, null, file.getPath(), datasetFolder.getName()))
+                    dbManager.insertImagePath(filename, file.getPath(), true);
+
+            } else {
+                if (!dbManager.insertSyncedImage(filename, classificationLabel, file.getPath(), null, datasetFolder.getName()))
+                    dbManager.insertImagePath(filename, file.getPath(), false);
+            }
+
+            return true;
+        } catch (Exception e) {
+            Log.e("TAG", "Error: " + e.getMessage());
+        }
+
+        return false;
+    }
+
+    /***
+     * Gets the directory to store a particular dataset's full images or its image cache.
+     * @param context the context
+     * @param dataset the name of the dataset (name to give subdirectory)
+     * @param isCache true if the folder should be a subdirectory of the cache folder
+     * @return the folder to store the images or null if the folder couldn't be created.
+     */
+    public static File getImageStorageDirectory(Context context, String dataset, boolean isCache) {
+        int userPK = new SessionManager(context).getUserPK();
+        String mainFolderName = (isCache) ? "UFDL_CACHE" : "UFDL";
+        File folder = new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), mainFolderName);
+
+        //create main directory if it doesn't exist
+        if (!folder.exists()) {
+            if (folder.mkdirs()) Log.e("TAG", "Created: " + mainFolderName);
+        }
+
+        //create dataset subdirectory
+        File datasetFolder = new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES) + File.separator + mainFolderName + File.separator + userPK, dataset);
+        if (!datasetFolder.exists()) {
+            if (datasetFolder.mkdirs()) return datasetFolder;
+        } else
+            return datasetFolder;
+
+        return null;
+    }
+
+    /***
+     * Gets the observer to set for the download dataset observable.
+     * @param progressBar the progress bar to display
+     * @param numFiles the number of files to download
+     * @return RxJava observer
+     */
+    private static Observer<Boolean> getDownloadObserver(ProgressBar progressBar, int numFiles) {
+        return new Observer<Boolean>() {
+            int currentProgress = 1;
+
+            @Override
+            public void onSubscribe(@io.reactivex.rxjava3.annotations.NonNull Disposable d) {
+                new Handler(Looper.getMainLooper()).post(() -> {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setMax(numFiles);
+                    progressBar.setSecondaryProgress(numFiles);
+                    progressBar.setProgress(currentProgress);
+                });
+            }
+
+            @Override
+            public void onNext(@io.reactivex.rxjava3.annotations.NonNull Boolean aBoolean) {
+                new Handler(Looper.getMainLooper()).post(() -> progressBar.setProgress(currentProgress++));
+            }
+
+            @Override
+            public void onError(@io.reactivex.rxjava3.annotations.NonNull Throwable e) {
+                Log.e("TAG", "Error when trying to download image:\n" + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                new Handler(Looper.getMainLooper()).post(() -> progressBar.setVisibility(View.GONE));
+            }
+        };
+    }
+
+    /***
+     * Compresses an image file.
+     * @param context the context
+     * @param file the file to compress
+     */
+    protected static void compressFile(Context context, File file) {
+        //create a compressed version of the image file
+        Compressor compressor = new Compressor(context);
+
+        try {
+            File compressedImage = compressor.compressToFile(file);
+            Log.e("TAG", compressedImage.getPath());
+
+            //replace original file with compressed file
+            Files.move(compressedImage.toPath(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+            //delete the parent folder of where the compressed image was initially stored
+            File parentFolder = compressedImage.getParentFile();
+            if (parentFolder != null && parentFolder.exists()) {
+                if (parentFolder.delete()) Log.e("TAG", "Successfully deleted parent folder");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /***
+     * Delete the full images and image cache of the dataset.
+     * @param context the context
+     * @param dataset the name of the dataset
+     * @throws IOException if deletion is unsuccessful
+     */
+    public static void deleteDatasetStorageDirectories(Context context, String dataset) throws IOException {
+        int userPK = new SessionManager(context).getUserPK();
+        File cacheFolder = new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES) + File.separator + "UFDL_CACHE" + File.separator + userPK, dataset);
+        File imageFolder = new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES) + File.separator + "UFDL" + File.separator + userPK, dataset);
+        File parent;
+
+        if (cacheFolder.exists() && (parent = cacheFolder.getParentFile()).list().length == 1)
+            FileUtils.deleteDirectory(parent);
+        else FileUtils.deleteDirectory(cacheFolder);
+
+        if (imageFolder.exists() && (parent = imageFolder.getParentFile()).list().length == 1)
+            FileUtils.deleteDirectory(parent);
+        else FileUtils.deleteDirectory(imageFolder);
+    }
+}
---
+++ app/src/main/java/io/github/waikato_ufdl/ImageOperations.java
@@ -0,0 +1,177 @@
+package io.github.waikato_ufdl;
+
+import android.util.Log;
+
+import com.github.waikatoufdl.ufdl4j.action.ImageClassificationDatasets;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+/***
+ * Encapsulates image operations for uploading, updating and deleting images from the local database and the server.
+ */
+
+public class ImageOperations {
+    /***
+     * If an Internet connection is available, this method will upload an image directly to the server. Else, the image will be uploaded
+     * to the local database with a sync status of CREATE.
+     * @param dbManager the database manager
+     * @param datasetPK the primary key of the dataset to store the image
+     * @param dataset the name of the dataset to store the image
+     * @param imageFile the image file to upload
+     * @param label the classification label of the image
+     */
+    public static void uploadImage(DBManager dbManager, int datasetPK, String dataset, File imageFile, String label) {
+        try {
+            if (SessionManager.isOnlineMode) {
+                ImageOperations.upload(dbManager, datasetPK, dataset, imageFile, label);
+            } else {
+                uploadToLocalDatabase(dbManager, dataset, imageFile.getName(), label, imageFile.getPath());
+            }
+        } catch (Exception e) {
+            uploadToLocalDatabase(dbManager, dataset, imageFile.getName(), label, imageFile.getPath());
+        }
+    }
+
+    /***
+     * Uploads an image to the local database with a sync status of CREATE.
+     * @param dbManager the database manager
+     * @param dataset the name of the dataset to store the image
+     * @param image the name of the image to upload
+     * @param label the classification label of the image
+     * @param path the file path of the image
+     */
+    protected static void uploadToLocalDatabase(DBManager dbManager, String dataset, String image, String label, String path) {
+        if (!dbManager.insertUnsyncedImage(image, label, path, null, dataset)) {
+            //set the sync status to synced & change label when the user attempts to upload images which are waiting to be synced (where the sync status = DELETE)
+            dbManager.setImageSynced(dataset, image);
+            dbManager.updateImageCategory(dataset, image, label);
+        }
+    }
+
+    /***
+     * Performs an API request to upload an image to the server. Then sets the sync status of the image to SYNCED in the local database.
+     * @param dbManager the database manager
+     * @param datasetPK the primary key of the dataset to store the image
+     * @param dataset the name of the dataset to store the image
+     * @param imageFile the image file to upload
+     * @param label the classification label of the image
+     * @throws Exception if API request fails
+     */
+    public static void upload(DBManager dbManager, int datasetPK, String dataset, File imageFile, String label) throws Exception {
+        ImageClassificationDatasets action = null;
+        try {
+            action = SessionManager.getClient().action(ImageClassificationDatasets.class);
+        } catch (Exception exception) {
+            Log.e("TAG", "Connection Failure");
+        }
+        //add the file to the server
+        if (action.addFile(datasetPK, imageFile, imageFile.getName())) {
+            //if the file has successfully been uploaded then update the image categories
+            if (action.addCategories(datasetPK, Collections.singletonList(imageFile.getName()), Collections.singletonList(label))) {
+                //set the image sync status to synced on the local database
+                dbManager.setImageSynced(dataset, imageFile.getName());
+            }
+        }
+    }
+
+    /***
+     * Reclassifies an image on the local database and then sets the sync status to UPDATE. Then an API request will be performed to reclassify the image on the server. If the request
+     * is successful, then the sync status of the image will be set to SYNCED in the local database.
+     * @param dbManager the database manager
+     * @param datasetPK the primary key of the dataset in which the image belongs
+     * @param dataset the name of the dataset in which the image belongs
+     * @param name the name of the image to update
+     * @param oldLabel the old classification label of the image
+     * @param newLabel the new classification label of the image
+     */
+    public static void updateImage(DBManager dbManager, int datasetPK, String dataset, String name, List<String> oldLabel, String newLabel) {
+        dbManager.reclassifyImage(dataset, name, newLabel);
+
+        if (SessionManager.isOnlineMode) {
+            update(dbManager, datasetPK, dataset, name, oldLabel, newLabel);
+        }
+    }
+
+    /***
+     * Performs an API request to reclassify an image on the server. If the request is successful, the sync status of the image will be set to SYNCED in the local database.
+     * @param dbManager the database manager
+     * @param datasetPK the primary key of the dataset in which the image belongs
+     * @param dataset the name of the dataset in which the image belongs
+     * @param name the name of the image to update
+     * @param oldLabel the old classification label of the image
+     * @param newLabel the new classification label of the image
+     */
+    public static void update(DBManager dbManager, int datasetPK, String dataset, String name, List<String> oldLabel, String newLabel) {
+        try {
+            ImageClassificationDatasets action = SessionManager.getClient().action(ImageClassificationDatasets.class);
+            //remove the old categories of the image and replace then with the new categories
+            if (action.removeCategories(datasetPK, Collections.singletonList(name), oldLabel)) {
+                if (action.addCategories(datasetPK, Collections.singletonList(name), Collections.singletonList(newLabel))) {
+                    //set the image status to synced
+                    dbManager.setImageSynced(dataset, name);
+                }
+            }
+        } catch (Exception e) {
+            Log.e("TAG", "API request failed -- Image Update Failed");
+        }
+    }
+
+    /***
+     * Sets the sync status of an image to DELETE on the local database and then an API request will be performed to delete the image on the server. If the request is successful,
+     * the image record will be deleted from the local database aand the cached image will be deleted from the device.
+     * @param dbManager the database manager
+     * @param datasetPK the primary key of the dataset in which the image belongs
+     * @param dataset the name of the dataset in which the image belongs
+     * @param image the name of the image to delete
+     * @param cachePath the cache path of the image to delete
+     */
+    public static void deleteImage(DBManager dbManager, int datasetPK, String dataset, String image, String cachePath) {
+        //set the image sync status to delete
+        dbManager.deleteImage(dataset, image);
+
+        //if we are in online mode, attempt to delete the image file from the server
+        if (SessionManager.isOnlineMode) {
+            delete(dbManager, datasetPK, dataset, image, cachePath);
+        }
+    }
+
+    /***
+     * Performs an API request to delete an image from the server. If the request is successful, delete the image record from the local database and deletes the cached image
+     * from the mobile device.
+     * @param dbManager the database manager
+     * @param datasetPK the primary key of the dataset in which the image belongs
+     * @param dataset the name of the dataset in which the image belongs
+     * @param image the name of the image to delete
+     * @param cachePath the cache path of the image to delete
+     */
+    public static void delete(DBManager dbManager, int datasetPK, String dataset, String image, String cachePath) {
+        try {
+            ImageClassificationDatasets action = SessionManager.getClient().action(ImageClassificationDatasets.class);
+            //if the image has been successfully deleted
+            if (action.deleteFile(datasetPK, image)) {
+                //set it's sync status to synced and then delete it's record from the local database
+                dbManager.setImageSynced(dataset, image);
+                String fullPath = dbManager.getImagePath(image, false);
+                boolean removed = dbManager.deleteSyncedImage(dataset, image);
+
+                //delete cached image
+                if (removed && cachePath != null) {
+                    File file = new File(cachePath);
+                    if (file.exists()) {
+                        Log.d("TAG", "Cache File Deleted: " + file.delete());
+                    }
+                }
+
+                //delete full image only if it is in the UFDL directory
+                if (removed && fullPath != null)
+                    if (fullPath.contains("UFDL") && fullPath.contains(dataset))
+                        new File(fullPath).delete();
+
+            }
+        } catch (Exception e) {
+            Log.e("TAG", "API Request Failed -- Image Deletion Failed\n" + e.getMessage());
+        }
+    }
+}
