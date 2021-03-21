ac7efe37e1747d8fe20f510357d640f7e09a4094
Yug Patel <yp61@students.waikato.ac.nz>
2021-03-22 02:48:03 +1300

    ClassifiedImage now implements the Cloneable interface.

--- app/src/main/java/io/github/waikato_ufdl/ui/images/ClassifiedImage.java
+++ app/src/main/java/io/github/waikato_ufdl/ui/images/ClassifiedImage.java
@@ -3,28 +3,14 @@ package io.github.waikato_ufdl.ui.images;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
-public class ClassifiedImage {
-    private byte[] image;
-    private String classification;
+public class ClassifiedImage implements Cloneable {
+    private final String classification;
     private boolean selected;
     private final String filename;
     private String filepath;
     private String cachePath;
     private String datasetName;
     private int syncStatus;
-
-
-    /***
-     * Constructor for a classified Image object
-     * @param image The byte array of the image
-     * @param label The image's classification
-     */
-    public ClassifiedImage(byte[] image, String label, String filename) {
-        this.image = image;
-        this.classification = label;
-        this.filename = filename;
-        selected = false;
-    }
 
     /***
      * Alternative constructor for a classified Image object
@@ -67,6 +53,14 @@ public class ClassifiedImage {
         this.syncStatus = syncStatus;
     }
 
+    protected Object clone() {
+        try {
+            return (ClassifiedImage) super.clone();
+        } catch (CloneNotSupportedException e) {
+            return new ClassifiedImage(this.filename, this.classification, this.filepath, this.cachePath);
+        }
+    }
+
     /***
      * getter method to get the dataset name in which the image belongs
      * @return the name of the dataset in which the image belongs
@@ -99,24 +93,12 @@ public class ClassifiedImage {
     public Bitmap getImageBitmap() {
         String filePath;
 
-        if (image != null) {
-            return BitmapFactory.decodeByteArray(image, 0, image.length);
-        }
-
         filePath = getFullImageFilePath();
         if (filePath == null) {
             filePath = getCachedFilePath();
         }
 
         return BitmapFactory.decodeFile(filePath);
-    }
-
-    /***
-     * Method to retrieve the image byte array
-     * @return image byte array
-     */
-    public byte[] getImageBytes() {
-        return image;
     }
 
     /***
@@ -150,14 +132,6 @@ public class ClassifiedImage {
      */
     public String getCachedFilePath() {
         return cachePath;
-    }
-
-    /***
-     * Method to set the cache path of the image
-     * @param path the path of the cached image
-     */
-    public void setCachePath(String path) {
-        cachePath = path;
     }
 
     /***
